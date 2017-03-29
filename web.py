79901006a9f88482bf8f8bc465271db18296eaa5
Mehmet Ali Gozaydin <kubudik@gmail.com>
2017-03-29 16:23:53 +0300

    add os, cast to int

--- web.py
+++ web.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
+import os
+
 
 app = Flask(__name__)
 
@@ -59,4 +61,5 @@ def sum():
         return "nok"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
